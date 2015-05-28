e582d20adb7ec1a0a6e35dc41427f7f2801a9194
Felix Chapman <aelred717@gmail.com>
2015-05-28 18:40:22 +0100

    new_list view tries to assign owner but cant

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -30,4 +30,30 @@ class MyListsTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
 
         self.browser.get(self.server_url)
-        self.wait_to_be_logged_in(email)
+        self.get_item_input_box().send_keys('Reticulate splines\n')
+        self.get_item_input_box().send_keys('Immanentize eschaton\n')
+        first_list_url = self.browser.current_url
+
+        # She notices a "My Lists" link, for the first time.
+        self.browser.find_element_by_link_text('My lists').click()
+
+        # She sees that her list is in there, named according to its
+        # first list item
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.assertEqual(self.browser.current_url, first_list_url)
+
+        # She decides to start another list, just to see
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('Click cows\n')
+        second_list_url = self.browser.current_url
+
+        # Under "muy lists", her new list appears
+        self.browser.find_element_by_link_text('My lists').click()
+        self.browser.find_element_by_link_text('Click cows').click()
+
+        # She logs out. The "My Lists" option disappears
+        self.browser.find_element_by_id('id_logout').click()
+        self.assertEqual(
+            self.browser.find_elements_by_link_text('My lists'),
+            []
+        )

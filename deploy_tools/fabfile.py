b3549c71c26bd7bfe835c706ed337a8789c5241c
Felix Chapman <aelred717@gmail.com>
2015-09-17 21:29:16 +0100

    Change deploy scripts to access requests from any domain with correct prefix

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -6,11 +6,12 @@ REPO_URL = 'git@github.com:aelred/superlists.git'
 
 
 def deploy():
-    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    prefix = env.host.split('.')[0]
+    site_folder = '/home/%s/sites/%s' % (env.user, prefix)
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
-    _update_settings(source_folder, env.host)
+    _update_settings(source_folder, env.host, prefix)
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
@@ -30,14 +31,10 @@ def _get_latest_source(source_folder):
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
 
-def _update_settings(source_folder, site_name):
+def _update_settings(source_folder, site_name, prefix):
     settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path, 'DOMAIN = "localhost"', 'DOMAIN = "%s"' % (site_name,))
-    sed(settings_path,
-        'ALLOWED_HOSTS =.+$',
-        'ALLOWED_HOSTS = ["%s"]' % (site_name,)
-    )
     secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
